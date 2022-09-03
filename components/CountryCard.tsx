51d491b15a0dd196828bf8df76dd407b67270cc6
Aldo Camacho <aldo@aldocamacho.com>
2022-09-02 20:24:33 -0700

    add countryCard

---
+++ components/CountryCard.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+import { CardPartialCountry } from "../types";
+
+interface CountryCardProps {
+  country: CardPartialCountry;
+}
+
+const CardContainer = styled.article`
+  overflow: hidden;
+  border-radius: 5px;
+
+  box-shadow: 0 0 15px rgba(0, 0, 0, 15%);
+  background-color: ${({ theme }) => theme.colors.elements};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+const FlagContainer = styled.div`
+  position: relative;
+  height: 160px;
+`;
+
+const InfoContainer = styled.div`
+  margin: 25px;
+  margin-bottom: 45px;
+`;
+
+const CountryName = styled.p`
+  margin: 0;
+  margin-bottom: 15px;
+  font-weight: 800;
+  font-size: 16px;
+`;
+
+const InfoEntry = styled.p`
+  margin: 0;
+  margin-top: 5px;
+`;
+
+const InfoName = styled.em`
+  font-weight: 600;
+  font-style: normal;
+`;
+
+const CountryCard = ({ country }: CountryCardProps) => {
+  return (
+    <CardContainer>
+      <FlagContainer>
+        <Image
+          src={country.flags.svg}
+          alt={`${country.name.common}'s capital`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </FlagContainer>
+      <InfoContainer>
+        <CountryName>{country.name.common}</CountryName>
+        <InfoEntry>
+          <InfoName>Population: </InfoName>
+          {country.population.toLocaleString(undefined)}
+        </InfoEntry>
+        <InfoEntry>
+          <InfoName>Region: </InfoName>
+          {country.region}
+        </InfoEntry>
+        <InfoEntry>
+          <InfoName>Capital: </InfoName>
+          {country.capital[0]}
+        </InfoEntry>
+      </InfoContainer>
+    </CardContainer>
+  );
+};
+
+export default CountryCard;
---
+++ types/index.ts
@@ -0,0 +1,76 @@
+interface Country {
+  name: {
+    common: string;
+    official: string;
+    nativeName: Record<string, NativeName>;
+  };
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: Record<string, Currency>;
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: Record<string, string>;
+  translations: Record<string, NativeName>;
+  latlng: number[];
+  demonyms: Record<string, Demonym>;
+  flag: string;
+  maps: {
+    googleMaps: string;
+    openStreetMaps: string;
+  };
+  population: number;
+  gini: Record<string, number>;
+  fifa: string;
+  car: {
+    signs: string[];
+    side: string;
+  };
+  timezones: string[];
+  continents: string[];
+  flags: Pictures;
+  coatOfArms: Pictures;
+  capitalInfo: {
+    latlng: number[];
+  };
+  postalCoda: {
+    format: string;
+    regex: string;
+  };
+}
+
+interface NativeName {
+  official: string;
+  common: string;
+}
+
+interface Currency {
+  name: string;
+  symbol: string;
+}
+
+interface Demonym {
+  f: string;
+  m: string;
+}
+
+interface Pictures {
+  png: string;
+  svg: string;
+}
+
+export type CardPartialCountry = Pick<
+  Country,
+  "name" | "flags" | "cca3" | "capital" | "region" | "population"
+>;
