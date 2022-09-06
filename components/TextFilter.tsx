6475f66188a0a4fa3affd326f4e6641da6fc2699
Aldo Camacho <aldo@aldocamacho.com>
2022-09-06 09:28:28 -0700

    style search box

---
+++ components/TextFilter.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+interface TextFilterProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 28px;
+
+  background-color: ${({ theme }) => theme.colors.elements};
+  color: ${({ theme }) => theme.colors.text};
+  border: unset;
+  border-radius: 8px;
+  width: 480px;
+`;
+
+const Input = styled.input`
+  padding: 0;
+  flex: 1;
+  border: none;
+  outline: none;
+  background-color: unset;
+  padding-left: 20px;
+
+  color: ${({ theme }) => theme.colors.text};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.text};
+  }
+`;
+
+const Icon = styled.span`
+  font-size: 30px;
+`;
+
+const TextFilter = ({ value, onChange }: TextFilterProps) => {
+  return (
+    <Container>
+      <Icon className="material-symbols-outlined">search</Icon>
+      <Input
+        type="text"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search for a country..."
+      />
+    </Container>
+  );
+};
+
+export default TextFilter;
